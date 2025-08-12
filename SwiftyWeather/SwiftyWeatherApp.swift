b810a11fc243960790c7db04fd7b5367a918224f
dferruggia <donf@warwick.net>
2025-08-11 21:41:10 -0400

    Initial Commit

---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Don Ferruggia on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
